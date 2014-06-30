93e1889b58b617ef0cb643a5d83d7588eeb95296
barthel <barthel@users.noreply.github.com>
2014-06-30 08:09:20 +0200

    rename debug into verbose mode; merge option added

--- dot_dependencies.sh
+++ dot_dependencies.sh
@@ -12,13 +12,15 @@
 # @see: https://maven.apache.org/plugins/maven-dependency-plugin/tree-mojo.html
 
 #set -x
-debug=0
+verbose=0
 
 input_files=('pom.xml')
 output_file='dependencies.dot'
 # filter for atifacts
 includes='de.icongmbh.*:::'
 excludes=''
+
+merge_dependencies=0
 
 # page size; include in DOT file as page="..."; default DIN A 4
 page_size='8.3,11.7'
@@ -48,25 +50,28 @@ exec_cut_console_message='cut -d"]" -f2'
 
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-dhsu [-e EXCLUDES] [-i INCLUDES] [-o OUTFILE] [-p PAGE_SIZE] [FILE]...
+Usage: ${0##*/} [-hmsuv [-e EXCLUDES] [-i INCLUDES] [-o OUTFILE] [-p PAGE_SIZE] [FILE]...
 Create a DOT file based on Maven dependencies (as a 'subgraph') provided by FILE.
 
 With no FILE the default '$input_files' will be used.
     
-    -d           debug option
-    -e EXCLUDES  A comma-separated list of artifacts to filter from the serialized dependency tree, or null (default) not
+    -e EXCLUDES  exclude dependencies mode.
+                 A comma-separated list of artifacts to filter from the serialized dependency tree, or null (default) not
                  to filter any artifacts from the dependency tree. An empty pattern segment is treated as an implicit wildcard.
     -h           display this help and exit
-    -i INCLUDES  A comma-separated list of artifacts to filter the serialized dependency tree by, or null not
+    -i INCLUDES  include dependencies mode.
+                 A comma-separated list of artifacts to filter the serialized dependency tree by, or null not
                  to filter the dependency tree. An empty pattern segment is treated as an implicit wildcard.
                  '$includes' (default)
                  See: https://maven.apache.org/plugins/maven-dependency-plugin/tree-mojo.html#includes for more information
+    -m           merge dependencies mode.
     -o OUTFILE   Write the result to OUTFILE instead of '$output_file' (default).
     -p PAGE_SIZE The page size in inch.
                  See: http://www.graphviz.org/content/attrs#dpage for more information
-    -s           ONLY SNAPSHOT versions
-    -u           Forces a check for updated releases and snapshots on remote
-                 Maven repositories
+    -s           ONLY SNAPSHOT versions mode
+    -u           force update repositories mode.
+                 Forces a check for updated releases and snapshots on remote Maven repositories
+    -v           verbose mode. Can be used multiple times for increased verbosity.
     
 Example: ${0##*/} \`find . -mindepth 2 -iname pom.xml | grep -v "target"\`
 EOF
@@ -76,11 +81,9 @@ EOF
 # process command line arguments
 # @see: http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash#192266
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
-while getopts "deh?iopsu:" opt;
+while getopts "eh?imopsuv" opt;
 do
     case "$opt" in
-    d)  debug=$((debug + 1))
-        ;;
     e)  exec_mvn="$exec_mvn -Dexcludes=\"$OPTARG\""
         ;;
     h|\?)
@@ -89,6 +92,8 @@ do
         ;;
     i)  includes="$OPTARG"
         ;;
+    m)  merge_dependencies=1
+        ;;
     o)  output_file=$OPTARG
         ;;
     p)  page_size="$OPTARG"
@@ -96,6 +101,8 @@ do
     s)  include="$include*-SNAPSHOT"
         ;;
     u)  exec_mvn="$mvn_exec -U"
+        ;;
+    v)  verbose=$((verbose + 1))
         ;;
     esac
 done
@@ -110,8 +117,8 @@ then
   input_files=("$@")
 fi
 
-# add Maven debug option if 'd' command line arg iwas more than once
-[[ $debug -gt 1 ]] && exec_mvn="$exec_mvn -X"
+# add Maven verbose option if 'd' command line arg iwas more than once
+[[ $verbose -gt 1 ]] && exec_mvn="$exec_mvn -X"
 ### CMD ARGS
 
 ### DEPENDENCIES
@@ -127,7 +134,7 @@ do
   #    mvn_cmd="$exec_mvn -DoutputFile=$temp_output_file -Doutput=$temp_output_file -Dincludes=\"$include\" -f \"$pom_file\" "
   # use the console output instead
   mvn_cmd="$exec_mvn -Dincludes=\"$includes\" -f\"$pom_file\" 2>&1 | $exec_grep_filter_console_message | $exec_cut_console_message"
-  [[ $debug -gt 0 ]] && echo "$mvn_cmd"
+  [[ $verbose -gt 0 ]] && echo "$mvn_cmd"
   eval $mvn_cmd >> $temp_output_file
   [[ $? -gt 0 ]] && exit $?; # check the return value
   counter=$((counter + 1))
@@ -140,17 +147,27 @@ then
 fi
 ### DEPENDENCIES
 
-### CLEAN UP
 echo "create: $output_file"
 echo -e 'digraph G { \n ' > $output_file
 echo -e "    graph [fontsize=8 fontname=\"Courier\" compound=true];\n    node [shape=record fontsize=8 fontname=\"Courier\"];\n    rankdir=\"LR\";\n    page=\"$page_size\";\n " >> $output_file
-# cleanup and normalize DOT content
-cmd_cleanup="$exec_sed_rename_graph $temp_output_file | $exec_sed_normalize_artifacts | $exec_awk_duplicate_lines | $exec_sed_duplicate_braces_line"
-[[ $debug -gt 0 ]] && echo "$cmd_cleanup"
-eval $cmd_cleanup >> $output_file
+cmd="$exec_sed_rename_graph $temp_output_file | $exec_sed_normalize_artifacts"
+if [ $merge_dependencies -lt 1 ]
+then
+  cmd="$cmd"
+  [[ $verbose -gt 0 ]] && echo "$cmd"
+  eval $cmd >> $output_file
+else
+### MERGE and CLEAN UP
+  echo 'merge and clean up dependencies'
+  # cleanup and normalize DOT content
+  cmd="$cmd | $exec_awk_duplicate_lines | $exec_sed_duplicate_braces_line"
+  [[ $verbose -gt 0 ]] && echo "$cmd"
+  eval $cmd >> $output_file
+### MERGE and CLEAN UP
+fi
 echo '}' >> $output_file
 
-# clean up temp. work file if debug level is lower than '2'
+# clean up temp. work file if verbose level is lower than '2'
 # @see: http://www.linuxjournal.com/content/use-bash-trap-statement-cleanup-temporary-files
-[[ $debug -lt 2 ]] && trap "rm -f $temp_output_file" EXIT
+[[ $verbose -lt 2 ]] && trap "rm -f $temp_output_file" EXIT
 
