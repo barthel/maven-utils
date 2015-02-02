34537ac48041c56e7bbc3a567ad3afa6663bd1f9
barthel <barthel@users.noreply.github.com>
2015-02-02 08:04:21 +0100

    Add script to deploy missing source artifacts.

---
+++ deploy_missing_sources_artefacts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Deploy all *-sources.jar files located in export directory into Maven Repository defined by URL.
+
+set -m
+#set -x
+# commands
+grep_cmd="grep --color=never -E "
+
+# required
+base_directory="$(readlink -f $(pwd))/"
+export_directory="$(readlink -f ${base_directory}./export/)"
+
+maven_repository_id="distribution-icon-development-releases"
+maven_repository_url="http://archiva.icongmbh.de/archiva/repository/icon-development-releases/"
+
+for jar in $(find ${export_directory} -name *-sources.jar)
+do
+  echo ${jar}
+  path=$(dirname ${jar})
+  pom_path=$(dirname ${path})
+  filename="${jar##*\/}"
+  if [ -e "${pom_path}/./pom.xml" ]
+  then
+    pom_file="${pom_path}/./pom.xml"
+    groupId=$(mvn help:evaluate -f ${pom_file} -Dexpression=project.groupId | grep --color=never -Ev '(^\[|WARNING|Download\w+:)')
+    artifactId=$(mvn help:evaluate -f ${pom_file} -Dexpression=project.artifactId | grep --color=never -Ev '(^\[|WARNING|Download\w+:)')
+    version=$(mvn help:evaluate -f ${pom_file} -Dexpression=project.version | grep --color=never -Ev '(^\[|WARNING|Download\w+:)')
+    mvn -q -Dfile=${jar} deploy:deploy-file -DrepositoryId=${maven_repository_id} -Durl=${maven_repository_url} -DgeneratePom=false -DartifactId=${artifactId} -DgroupId=${groupId} -Dpackaging=jar -Dclassifier=sources -Dversion=${version} -DupdateReleaseInfo=true
+  fi
+done
