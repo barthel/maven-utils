25656744c4dfcef70444014856d1db1e664ee6ae
barthel <barthel@users.noreply.github.com>
2015-03-13 15:39:48 +0100

    set-dependency-version-in-all-poms.sh corrected. Maven bash completion.
    Use single quote instead of double quote in bash script.
    Add Maven bash completion as submodule.

--- set-dependency-version-in-all-poms.sh
+++ set-dependency-version-in-all-poms.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for file in $(find . -name \*pom.xml | grep -v target)
-do
-  versionString="${1}.version"
-  version="${2}"
-  sed -i "s+<${versionString}>.*</${versionString}>+<${versionString}>${version}</${versionString}>+" $file
-done
+versionString="${1}.version"
+version="${2}"
+
+# it seems it's faster then a for-loop on cygwin ???
+find . -name '*pom.xml' | grep -v 'target\|bin' | xargs sed -i "s+<${versionString}>.*</${versionString}>+<${versionString}>${version}</${versionString}>+"
+
