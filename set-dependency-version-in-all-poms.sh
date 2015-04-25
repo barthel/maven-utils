211095d19788c42a1fccb667a6959e78b3952c09
barthel <barthel@users.noreply.github.com>
2015-04-25 13:06:22 +0200

    Refactoring set-dependency-version-in-all-poms.sh
    Escape dots in 'version' regexp pattern.
    Use the expressions provided by find instead of grep for name/path filter.
    Optimize sed inplace replace command. Use grouping matches for easier use
    of regex pattern in 'versionString'.
    Add small documentation.

--- set-dependency-version-in-all-poms.sh
+++ set-dependency-version-in-all-poms.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#
+# Replace version in POM file property entries following th pattern:
+#   <[artifactId].version>[version number]</[artifactId].version>
+#
 
-versionString="${1}.version"
-version="${2}"
+set -m
+# set -x
+
+# escape dot but not in {1} to use rexexp pattern into it
+versionString="${1}\.version"
+version="${2//\./\\.}"
 
 # it seems it's faster then a for-loop on cygwin ???
-find . -name '*pom.xml' | grep -v 'target\|bin' | xargs sed -i "s+<${versionString}>.*</${versionString}>+<${versionString}>${version}</${versionString}>+"
+# @see: http://stackoverflow.com/questions/7573368/in-place-edits-with-sed-on-os-x
+# meaning of parameters in ordered way
+# find relative path only of files with name pattern and the path does not contains one of the path pattern
+# select file names containing the version string
+# inline replace of version
+find . -type f \( -name '*pom.xml' -and -not -ipath '*/.git/*' -and -not -ipath '*/target/*' -and -not -ipath '*/bin/*' \) | xargs grep -l "<${versionString}>" | xargs sed -i '' "s|<\(${versionString}\)>\(.*\)<|<\1>${version}<|"
 
